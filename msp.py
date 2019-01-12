b39556e551b0855a5ab00ebb00cd4576f190db57
Andrei Ciubotariu <andrei.development@gmail.com>
2019-01-12 14:02:29 -0500

    wip

--- msp.py
+++ msp.py
@@ -3,6 +3,7 @@ from construct import Struct, Const, Int8ul, Int32ul
 import struct
 import time
 
+MSP_SERIAL_BAUD = 115200
 MSP_SERIAL_TIMEOUT = 2
 
 # Message IDs (commands)
@@ -10,12 +11,12 @@ MSP_IDENT = 100
 
 
 MSP_PREAMBLE = b'$M'
-MSP_DIRECTION_FROM_FLIGHT_CONTROLLER = b'>'
-MSP_DIRECTION_TO_FLIGHT_CONTROLLER = b'<'
+MSP_DIR_FROM_BOARD = b'>'
+MSP_DIR_TO_BOARD = b'<'
 
 MSP_CONSTRUCTS = {
 	MSP_IDENT : Struct('preamble' / Const(MSP_PREAMBLE),
-						  'direction' / Const(MSP_DIRECTION_FROM_FLIGHT_CONTROLLER),
+						  'direction' / Const(MSP_DIR_FROM_BOARD),
 						  'size' / Int8ul,
 						  'message_id' / Const(100, Int8ul),
 						  'version' / Int8ul,
@@ -29,13 +30,15 @@ class MSP:
 
 	def __init__(self, port, serial_delay=15):
 		self.serial = Serial(port=port, 
-							 baudrate=115200,
+							 baudrate=MSP_SERIAL_BAUD,
 							 timeout=MSP_SERIAL_TIMEOUT)
 		print('Waiting {0} seconds for board to wakeup'.format(serial_delay))
 		time.sleep(serial_delay)
 		print('Done')
 
 	def send_data(self, message_id, data):
+		# TODO send data
+		# TODO wait for ack back
 		pass
 
 	def send_construct(self, cmd, data):
@@ -44,7 +47,7 @@ class MSP:
 	def request_info(self, message_id):
 		# An information request does not contain any 'data', so the CRC is simply the message_id
 		request = Struct('preamble' / Const(MSP_PREAMBLE),
-			  'direction' / Const(MSP_DIRECTION_TO_FLIGHT_CONTROLLER),
+			  'direction' / Const(MSP_DIR_TO_BOARD),
 			  'size' / Const(0, Int8ul),
 			  'message_id' / Const(message_id, Int8ul),
 			  'crc' / Const(message_id, Int8ul)) 
