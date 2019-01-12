d0b32b7ab4c9109f7f989d3a1ce68eb97be9afb4
Andrei Ciubotariu <andrei.development@gmail.com>
2019-01-12 13:07:09 -0500

    Add initial functionality
    - Communication to/from the board works
    - A few commands implemented.

---
+++ msp.py
@@ -0,0 +1,177 @@
+# TODO
+# Change lan and lon to Int32. That's how they're stored in the mission_step struct
+
+from serial import Serial
+from construct import Struct, Const, Int8ul, Int16ul, Int32ul
+import struct
+import time
+
+MSP_SERIAL_BAUD = 115200
+MSP_SERIAL_TIMEOUT = 5
+
+# Message IDs (commands)
+MSP_IDENT = 100
+MSP_GET_WP = 118
+MSP_NAV_STATUS = 121
+MSP_SET_WP = 209
+
+MSP_PREAMBLE = b'$M'
+MSP_DIR_FROM_BOARD = b'>'
+MSP_DIR_TO_BOARD = b'<'
+MSP_DATASIZE_INDEX = len(MSP_PREAMBLE + MSP_DIR_TO_BOARD)
+
+MSP_ACK = Struct('preamble' / Const(MSP_PREAMBLE),
+                 'direction' / Const(MSP_DIR_FROM_BOARD),
+                 'size' / Const(0, Int8ul),
+                 'message_id' / Int8ul,
+                 'crc' / Int8ul)
+
+
+MSP_SETTINGS_PROVIDERS = {
+    MSP_SET_WP : Struct('preamble' / Const(MSP_PREAMBLE),
+                        'direction' / Const(MSP_DIR_TO_BOARD),
+                        'size' / Const(21, Int8ul),
+                        'message_id' / Const(MSP_SET_WP, Int8ul),
+                        'wp_no' / Int8ul,
+                        'action' / Int8ul,
+                        'lat' / Int32ul,
+                        'lon' / Int32ul,
+                        'altitude' / Int32ul,
+                        'param1' / Int16ul,
+                        'param2' / Int16ul,
+                        'param3' / Int16ul,
+                        'flag' / Int8ul),
+}
+
+MSP_PARAMETERIZED_REQUESTS = {
+    MSP_GET_WP : Struct('preamble' / Const(MSP_PREAMBLE),
+                        'direction' / Const(MSP_DIR_TO_BOARD),
+                        'size' / Const(1, Int8ul),
+                        'message_id' / Const(MSP_GET_WP, Int8ul),
+                        'wp_no' / Int8ul),
+}
+
+MSP_REQUEST_RESPONSES = {
+    MSP_IDENT : Struct('preamble' / Const(MSP_PREAMBLE),
+                       'direction' / Const(MSP_DIR_FROM_BOARD),
+                       'size' / Const(7, Int8ul),
+                       'message_id' / Const(MSP_IDENT, Int8ul),
+                       'version' / Int8ul,
+                       'multitype' / Int8ul,
+                       'msp_version' / Int8ul,
+                       'capability' / Int32ul,
+                       'crc' / Int8ul),
+    MSP_GET_WP : Struct('preamble' / Const(MSP_PREAMBLE),
+                        'direction' / Const(MSP_DIR_FROM_BOARD),
+                        'size' / Const(21, Int8ul),
+                        'message_id' / Const(MSP_GET_WP, Int8ul),
+                        'wp_no' / Int8ul,
+                        'action' / Int8ul,
+                        'lat' / Int32ul,
+                        'lon' / Int32ul,
+                        'altitude' / Int32ul,
+                        'param1' / Int16ul,
+                        'param2' / Int16ul,
+                        'param3' / Int16ul,
+                        'flag' / Int8ul,
+                        'crc' / Int8ul),
+    MSP_NAV_STATUS : Struct('preamble' / Const(MSP_PREAMBLE),
+                            'direction' / Const(MSP_DIR_FROM_BOARD),
+                            'size' / Const(7, Int8ul),
+                            'message_id' / Const(MSP_NAV_STATUS, Int8ul),
+                            'gps_mode' / Int8ul,
+                            'nav_state' / Int8ul,
+                            'curr_mission_action' / Int8ul,
+                            'curr_mission_number' / Int8ul,
+                            'nav_error' / Int8ul,
+                            'target_bearing' / Int16ul,
+                            'crc' / Int8ul),
+}
+
+class MSP:
+
+    def __init__(self, port='/dev/ttyUSB0', serial_delay=15):
+        self.serial = Serial(port=port,
+                             baudrate=MSP_SERIAL_BAUD,
+                             timeout=MSP_SERIAL_TIMEOUT)
+        print('Waiting {0} seconds for board to wake up'.format(serial_delay))
+        time.sleep(serial_delay)
+        print('Done waiting')
+
+    def calc_crc(self, data):
+        crc = 0
+        for a_byte in data:
+            crc ^= a_byte
+        return crc
+
+    def provide(self, message_id, parameters):
+        self.send_construct(MSP_SETTINGS_PROVIDERS[message_id], parameters);
+
+    def read_ack(self, message_id):
+        ack = self.receive_data(MSP_ACK)
+        if ack.message_id != message_id:
+            raise ValueError("Received ACK for {0} but expected {1}".format(ack.message_id, message_id))
+
+    def send_construct(self, cmd, parameters):
+        data = cmd.build(parameters)
+        crc = self.calc_crc(data[MSP_DATASIZE_INDEX::])
+        data += crc.to_bytes(1, byteorder='little')
+        self.serial.write(data)
+
+    def receive_data(self, parser):
+        received_data = self.read(parser.sizeof())
+        crc = self.calc_crc(received_data[MSP_DATASIZE_INDEX:-1])
+        try:
+            parsed_data = parser.parse(received_data)
+        except:
+            print("received_data: ", received_data)
+            raise
+
+        if (crc != parsed_data.crc):
+            raise ValueError("CRC does not match. Expected {0} but got {1}. {2}".format(crc, parsed_data.crc, parsed_data))
+
+        return parsed_data
+
+    def request(self, message_id, parameters={}):
+        request = MSP_PARAMETERIZED_REQUESTS.get(message_id,
+                                                 Struct('preamble' / Const(MSP_PREAMBLE),
+                                                        'direction' / Const(MSP_DIR_TO_BOARD),
+                                                        'size' / Const(0, Int8ul),
+                                                        'message_id' / Const(message_id, Int8ul)))
+        self.send_construct(request, parameters)
+        return self.receive_data(MSP_REQUEST_RESPONSES[message_id])
+
+    def read(self, num_bytes):
+        return self.serial.read(num_bytes)
+
+    def close(self):
+        self.serial.close()
+
+if __name__ == '__main__':
+    serial_port = "/dev/ttyUSB0"
+
+    msp = MSP(serial_port)
+
+    print(msp.request(MSP_IDENT))
+    print(msp.request(MSP_GET_WP, {'wp_no': 0}))
+    print(msp.request(MSP_GET_WP, {'wp_no': 5}))
+
+    print('Send waypoint')
+    msp.provide(MSP_SET_WP,
+                 {
+                    'wp_no'  : 1,
+                    'action' : 1,
+                    'lat' : 4,
+                    'lon' : 57,
+                    'altitude' : 9,
+                    'param1' : 1,
+                    'param2' : 5,
+                    'param3' : 4,
+                    'flag' : 0,
+                 })
+    msp.read_ack(MSP_SET_WP)
+    print('Get same waypoint')
+    print(msp.request(MSP_GET_WP, {'wp_no': 1}))
+    print(msp.request(MSP_NAV_STATUS))
+
+    msp.close()
---
+++ set_waypoints.py
@@ -0,0 +1,76 @@
+import sys
+
+from msp import MSP, MSP_SET_WP, MSP_GET_WP
+
+COMMENT_START_CHAR = '#'
+
+MSP_WAYPOINT_ACTIONS = {
+    "MISSION_WAYPOINT"     : 1,   # Set waypoint
+    "MISSION_HOLD_UNLIM"   : 2,   # Poshold unlimited
+    "MISSION_HOLD_TIME"    : 3,   # Hold for a predetermined time
+    "MISSION_RTH"          : 4,   # Return to HOME
+    "MISSION_SET_POI"      : 5,   # Set POINT of interest
+    "MISSION_JUMP"         : 6,   # Jump to the given step (#times)
+    "MISSION_SET_HEADING"  : 7,   # Set heading to a given orientation (parameter 1 is the waym 0-359 degree
+    "MISSION_LAND"         : 8,   # Land at the given position
+}
+
+# Commented out flags seem to be internal to the flight controller
+MSP_WAYPOINT_FLAGS = {
+    "MISSION_FLAG_NONE"        : 0x00,
+    "MISSION_FLAG_END"         : 0xA5, # Flags that this is the last step
+    # "MISSION_FLAG_CRC_ERROR"   : 0xFE, # Returned WP had an EEPROM CRC error
+    "MISSION_FLAG_HOME"        : 0x01, # Returned WP is the home position
+    "MISSION_FLAG_HOLD"        : 0x02, # Returned WP is the hold position
+    "MISSION_FLAG_DO_LAND"     : 0x20, # Land when reached desired point (used in RTH)
+    # "MISSION_FLAG_NAV_IN_PROG" : 0xff, # Navigation is in progress, returned wp is home
+}
+
+# Indices in the waypoint string
+IDX_LAT = 0
+IDX_LON = 1
+IDX_ACTION = 2
+IDX_ALTITUDE = 3
+IDX_PARAM1 = 4
+IDX_PARAM2 = 5
+IDX_PARAM3 = 6
+IDX_FLAG = 7
+
+MAX_WAYPOINTS = 255
+
+def send_waypoint(protocol, params):
+    protocol.provide(MSP_SET_WP, params)
+    protocol.read_ack(MSP_SET_WP)
+
+if __name__ == '__main__':
+    filename = 'waypoints.txt'
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+
+    print('Using {0}'.format(filename))
+    with open(filename) as f:
+        protocol = MSP()
+        for i in range(0,MAX_WAYPOINTS):
+            wp_no = i+1
+
+            line = f.readline().strip()
+            if not line:
+                break
+            waypoint = line.split(' ')
+            if waypoint[0].startswith(COMMENT_START_CHAR):
+                continue
+
+            print(waypoint)
+            send_waypoint(protocol,
+                {
+                    'wp_no'  : wp_no,
+                    'action' : MSP_WAYPOINT_ACTIONS[waypoint[IDX_ACTION]],
+                    'lat' : int(waypoint[IDX_LAT]),
+                    'lon' : int(waypoint[IDX_LON]),
+                    'altitude' : int(waypoint[IDX_ALTITUDE]),
+                    'param1' : int(waypoint[IDX_PARAM1]),
+                    'param2' : int(waypoint[IDX_PARAM1]),
+                    'param3' : int(waypoint[IDX_PARAM1]),
+                    'flag' : MSP_WAYPOINT_FLAGS[waypoint[IDX_FLAG]],
+                })
+            print(protocol.request(MSP_GET_WP, {'wp_no': wp_no}))
