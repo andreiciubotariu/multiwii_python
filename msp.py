8d7956b0f827f14b6fc76628ce72a6247e7eba0f
Andrei Ciubotariu <andrei.development@gmail.com>
2019-01-13 11:04:06 -0500

    convert indentation to spaces

--- msp.py
+++ msp.py
@@ -17,97 +17,97 @@ MSP_DIR_TO_BOARD = b'<'
 MSP_DATASIZE_INDEX = len(MSP_PREAMBLE + MSP_DIR_TO_BOARD)
 
 MSP_PARAMETERIZED_REQUESTS = {
-	MSP_GET_WP : Struct('preamble' / Const(MSP_PREAMBLE),
-					    'direction' / Const(MSP_DIR_TO_BOARD),
-						'size' / Const(1, Int8ul),
-						'message_id' / Const(MSP_GET_WP, Int8ul),
-						'wp_no' / Int8ul),
+    MSP_GET_WP : Struct('preamble' / Const(MSP_PREAMBLE),
+                        'direction' / Const(MSP_DIR_TO_BOARD),
+                        'size' / Const(1, Int8ul),
+                        'message_id' / Const(MSP_GET_WP, Int8ul),
+                        'wp_no' / Int8ul),
 }
 
 MSP_RECEIVE_CONSTRUCTS = {
-	MSP_IDENT : Struct('preamble' / Const(MSP_PREAMBLE),
-						  'direction' / Const(MSP_DIR_FROM_BOARD),
-						  'size' / Int8ul,
-						  'message_id' / Const(MSP_IDENT, Int8ul),
-						  'version' / Int8ul,
-						  'multitype' / Int8ul,
-						  'msp_version' / Int8ul,
-						  'capability' / Int32ul,
-						  'crc' / Int8ul),
+    MSP_IDENT : Struct('preamble' / Const(MSP_PREAMBLE),
+                          'direction' / Const(MSP_DIR_FROM_BOARD),
+                          'size' / Int8ul,
+                          'message_id' / Const(MSP_IDENT, Int8ul),
+                          'version' / Int8ul,
+                          'multitype' / Int8ul,
+                          'msp_version' / Int8ul,
+                          'capability' / Int32ul,
+                          'crc' / Int8ul),
 
-	MSP_GET_WP : Struct('preamble' / Const(MSP_PREAMBLE),
-					    'direction' / Const(MSP_DIR_FROM_BOARD),
-						'size' / Int8ul,
-						'message_id' / Const(MSP_GET_WP, Int8ul),
-						'wp_no' / Int8ul,
-						'lat' / Int32ul,
-						'lon' / Int32ul,
-						'altitude_hold' / Int32ul,
-						'heading' / Int16ul,
-						'time_to_stay' / Int16ul,
-						'crc' / Int8ul),
+    MSP_GET_WP : Struct('preamble' / Const(MSP_PREAMBLE),
+                        'direction' / Const(MSP_DIR_FROM_BOARD),
+                        'size' / Int8ul,
+                        'message_id' / Const(MSP_GET_WP, Int8ul),
+                        'wp_no' / Int8ul,
+                        'lat' / Int32ul,
+                        'lon' / Int32ul,
+                        'altitude_hold' / Int32ul,
+                        'heading' / Int16ul,
+                        'time_to_stay' / Int16ul,
+                        'crc' / Int8ul),
 }
 
 class MSP:
 
-	def __init__(self, port, serial_delay=15):
-		self.serial = Serial(port=port, 
-							 baudrate=MSP_SERIAL_BAUD,
-							 timeout=MSP_SERIAL_TIMEOUT)
-		print('Waiting {0} seconds for board to wakeup'.format(serial_delay))
-		time.sleep(serial_delay)
-		print('Done')
+    def __init__(self, port, serial_delay=15):
+        self.serial = Serial(port=port, 
+                             baudrate=MSP_SERIAL_BAUD,
+                             timeout=MSP_SERIAL_TIMEOUT)
+        print('Waiting {0} seconds for board to wakeup'.format(serial_delay))
+        time.sleep(serial_delay)
+        print('Done')
 
 
-	def calc_crc(self, data):
-		crc = 0
-		for a_byte in data:
-			crc ^= a_byte
-		return crc
+    def calc_crc(self, data):
+        crc = 0
+        for a_byte in data:
+            crc ^= a_byte
+        return crc
 
-	def send_data(self, message_id, data):
-		# TODO send data
-		# TODO wait for ack back
-		pass
+    def send_data(self, message_id, data):
+        # TODO send data
+        # TODO wait for ack back
+        pass
 
-	def send_construct(self, cmd, parameters):
-		data = cmd.build(parameters)
-		crc = self.calc_crc(data[MSP_DATASIZE_INDEX::])
-		print("CRC", crc)
-		data += crc.to_bytes(1, byteorder='little')
-		print(data)
-		print(len(data))
-		self.serial.write(data)
+    def send_construct(self, cmd, parameters):
+        data = cmd.build(parameters)
+        crc = self.calc_crc(data[MSP_DATASIZE_INDEX::])
+        print("CRC", crc)
+        data += crc.to_bytes(1, byteorder='little')
+        print(data)
+        print(len(data))
+        self.serial.write(data)
 
-	def request_info(self, message_id, parameters={}):
-		# By default return a struct that does not have any parameters. Since there is no data to transmit
-		# The crc is simply the message_id
-		# TODO actually calculate the CRC for all Structs. The parameterized requests need a CRC calculation
-		request = MSP_PARAMETERIZED_REQUESTS.get(message_id, 
-												 Struct('preamble' / Const(MSP_PREAMBLE),
-												 	    'direction' / Const(MSP_DIR_TO_BOARD),
-													    'size' / Const(0, Int8ul),
-													    'message_id' / Const(message_id, Int8ul)))
-		self.send_construct(request, parameters)
+    def request_info(self, message_id, parameters={}):
+        # By default return a struct that does not have any parameters. Since there is no data to transmit
+        # The crc is simply the message_id
+        # TODO actually calculate the CRC for all Structs. The parameterized requests need a CRC calculation
+        request = MSP_PARAMETERIZED_REQUESTS.get(message_id, 
+                                                 Struct('preamble' / Const(MSP_PREAMBLE),
+                                                        'direction' / Const(MSP_DIR_TO_BOARD),
+                                                        'size' / Const(0, Int8ul),
+                                                        'message_id' / Const(message_id, Int8ul)))
+        self.send_construct(request, parameters)
 
-		parser = MSP_RECEIVE_CONSTRUCTS[message_id]
+        parser = MSP_RECEIVE_CONSTRUCTS[message_id]
 
-		received_data = self.read(parser.sizeof())
-		crc = self.calc_crc(received_data[MSP_DATASIZE_INDEX:-1])
-		try:
-			parsed_data = parser.parse(received_data)
-		except:
-			print("received_data: ", received_data)
-			raise
+        received_data = self.read(parser.sizeof())
+        crc = self.calc_crc(received_data[MSP_DATASIZE_INDEX:-1])
+        try:
+            parsed_data = parser.parse(received_data)
+        except:
+            print("received_data: ", received_data)
+            raise
 
-		if (crc != parsed_data.crc):
-			raise ValueError("CRC does not match. Expected {0} but got {1}".format(crc, parsed_data.crc))
+        if (crc != parsed_data.crc):
+            raise ValueError("CRC does not match. Expected {0} but got {1}".format(crc, parsed_data.crc))
 
-		return parsed_data
+        return parsed_data
 
-	def read(self, num_bytes):
-		return self.serial.read(num_bytes)
-		
+    def read(self, num_bytes):
+        return self.serial.read(num_bytes)
+        
 
 serial_port = "/dev/ttyUSB0"
 
